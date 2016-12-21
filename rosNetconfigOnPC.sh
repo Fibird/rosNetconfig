8924cc544e7238e5d545bf75e464d47f5fb9ca18
Chaoyang Liu <chaoyanglius@gmail.com>
2016-12-21 20:12:04 +0800

    Improve these two scripts
    I do these things:
        * Add some important comments
        * Set ROS_IP using ifconfig

--- rosNetconfigOnPC.sh
+++ rosNetconfigOnPC.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+# Shell script scripts to set ROS network configuration
+# ----------------------------------------------------------------
+# Copyright(c)2016 Chaoyang Liu <http://chaoyangliu.cc>
+# This script is licensed under GNU GPL version 3 or above
+# ----------------------------------------------------------------
+
 echo "Load ROS network configuration..."
+
+# Get the hostname of remote robot side
 echo -n "Enter the hostname of Robot side: "
 read master_uri
+
 # The IP address for the Master node
 export ROS_MASTER_URI=http://${master_uri}.local:11311
 echo "The environment variable ROS_MASTER_URI is $ROS_MASTER_URI"
 ZChostname=`hostname`.local
+
 # The hostname for your device/host
 export ROS_HOSTNAME=$ZChostname
 echo "The environment variable ROS_HOSTNAME is $ROS_HOSTNAME"
+
+# Set ROS IP
+IP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'` 
+export ROS_IP=$IP
+echo "The environment ROS_IP is $ROS_IP"
+
 echo "ROS network configure successully!"
--- rosNetconfigOnRobot.sh
+++ rosNetconfigOnRobot.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+# Shell script scripts to set ROS network configuration
+# ----------------------------------------------------------------
+# Copyright(c)2016 Chaoyang Liu <http://chaoyangliu.cc>
+# This script is licensed under GNU GPL version 3 or above
+# ----------------------------------------------------------------
+
 echo "Load ROS network configuration..."
+
 # The localhost IP address = IP address for the Master node
 export ROS_MASTER_URI=http://localhost:11311
-# The IP address for the Master node
+
 ZChostname=`hostname`.local       # get the hostname
+# The hostname for the Master node
 export ROS_HOSTNAME=$ZChostname
 echo "The environment variable ROS_HOSTNAME is $ROS_HOSTNAME"
+
+# Set ROS IP
+IP=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'` 
+export ROS_IP=$IP
+echo "The environment ROS_IP is $ROS_IP"
+
 echo "ROS network configure successully!"
