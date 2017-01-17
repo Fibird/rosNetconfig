1d2acb4f9404d2dd4b9527e72386db271abb6411
Chaoyang Liu <chaoyanglius@gmail.com>
2017-01-17 22:22:27 +0800

    add necessary comments

--- rosNetconfigOnRobot.sh
+++ rosNetconfigOnRobot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Shell script scripts to set ROS network configuration
-# ----------------------------------------------------------------
+# Shell script scripts to set ROS network configuration on Robot side
+# -------------------------------------------------------------------
 # Copyright(c)2016 Chaoyang Liu <http://chaoyangliu.cc>
 # This script is licensed under GNU GPL version 3 or above
-# ----------------------------------------------------------------
+# -------------------------------------------------------------------
 
 echo "Load ROS network configuration..."
 
