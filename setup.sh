b795fed7c327a624f6d5918a70b40f4efb9dc2ac
Chaoyang Liu <chaoyanglius@gmail.com>
2017-04-22 18:06:45 +0800

    Close #11
    remove config file from repository and generate it by setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@
 # This script is licensed under GNU GPL version 3 or above
 # ----------------------------------------------------------------
 
+mkdir config/
+touch config/rosNetCfg      # create config file
+echo -e "export ROS_MASTER_URI=\nexport ROS_HOSTNAME=\nexport ROS_IP=" >> config/rosNetCfg
+# default mode is local mode
+./netConfig -l
 scriptPath=`pwd`
 echo "# ------------The settings produced by rosNetConfig------------" >> ~/.bashrc
 echo "source $scriptPath/config/rosNetCfg" >> ~/.bashrc
